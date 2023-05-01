699260488ab8cb70913b38fd95cf83e94f0182fa
DaOfficialWizard <45744329+ZanzyTHEbar@users.noreply.github.com>
2023-05-01 16:50:23 +0100

    fix: remove postcss

--- solid-three-template/postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-    purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    plugins: {
-        'postcss-import': {},
-        tailwindcss: {},
-        autoprefixer: {},
-    },
-}
